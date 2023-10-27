d25f3739810cb8d167f26389f2032734ee5f6b3e
Liam Biddle <biddleliam@outlook.com>
2023-10-27 18:10:13 +1100

    added main function

---
+++ src/fuzzer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import sys
+from types.csv import CSVFuzz
+
+from types.json import JSONFuzz
+
+if len(sys.arv) != 3:
+    print("Usage: ./fuzzer program sampleinput.txt")
+    sys.exit()
+
+
+try:
+    inputFile = open(sys.argv[2], 'r')
+    inputStr = inputFile.read().strip()
+
+
+except OSError:
+    print(f"Couldn't open input file: {sys.argv[2]}")
+    sys.exit()
+
+fuzz = None
+if JSONFuzz(inputStr).isType():
+    fuzz = JSONFuzz(inputStr)
+elif CSVFuzz(inputStr).isType():
+    fuzz = CSVFuzz(inputStr)
+else:
+    print(f"Couldn't parse input file")
+    sys.exit()
+    
+
+# fuzz.fuzz() ?
+
