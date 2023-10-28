14947b5566500e6db6420431b1267cfbbbd95d7e
Herzen <herzen@herzens-air.home>
2023-10-29 07:59:00 +1100

    add a working harness, did a little improvements on JSONMutator

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
- 
+from argparse import ArgumentParser
+from harness import Harness
+
+def main(binary, seed, times: int = 5000):
+    """
+    Initialize a harness with the given binary and seed, and then start it with a specified timeout.
+    """
+    runner = Harness(binary, seed)
+    runner.start(times) 
+    
+if __name__ == "__main__":
+    parser = ArgumentParser(description="A simple fuzzer")
+    parser.add_argument("binary", help="Path to the binary to be fuzzing tested")
+    parser.add_argument("seed", help="Seed value to be mutated")
+    parser.add_argument("--times", type=int, default=5000, help="Maximum testing times for the runner. Defaults to 5000.")
+    
+    args = parser.parse_args()
+    main(args.binary, args.seed, args.times)
