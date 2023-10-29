892bba082478306477960a2145776f5f414a84cb
Liam Biddle <biddleliam@outlook.com>
2023-10-29 14:25:58 +1100

    added path

--- src/runner.py
+++ src/runner.py
@@ -18,11 +18,8 @@ class Manager:
         self._num_runs = times
         self._current_checkpoint = 0
         
-        if "./"  != binary[1:2]:
-            self._process_name = f"./{binary}"
-        else:
-            self._process_name = f"{binary}"
-        
+        self._process_name = self._format_binary_path(binary)
+
         self._stop_flag = False
         
         try:
@@ -36,7 +33,6 @@ class Manager:
         self._file_type = process.whichType(self._input_file)
         self._fuzz = self._MUTATORS[self._file_type](self._input_file)
         self._init_process()
-
 
     def _format_binary_path(self, binary: str) -> str:
         """Ensure the binary path is correctly formatted."""
