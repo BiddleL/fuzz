7a92dde7b54129a198e48d992e84877158e133e9
Liam Biddle <biddleliam@outlook.com>
2023-11-19 12:18:52 +1100

    Ziqi final (#7)
    * basic process handling
    
    * added exit code enum
    
    * ?
    
    * fixed structure
    
    * complete mutator_base and csv_mutator_fuzzer
    
    * add a working harness, did a little improvements on JSONMutator
    
    * basic process handling
    
    * working in progress
    
    * rebase and cleaned up manager
    
    * trying to get working
    
    * name fix
    
    * updated  process package
    
    * import errors
    
    * fixed structure error
    
    * fixed fuzzer import error
    
    * fixed error
    
    * fml
    
    * bytes issue
    
    * same issue
    
    * mutators error
    
    * type issue
    
    * fuzzer
    
    * runner.py is fucked
    
    * removed pwn
    
    * process
    
    * help
    
    * fixed porcees init
    
    * enum issue
    
    * writing bytes
    
    * maybe fixed
    
    * Adding shafi-json to staging branch (#4)
    
    * json mutators initial code
    
    * json mutators initial code added
    
    * fix syntax error in json_mutator
    
    ---------
    
    Co-authored-by: xahfi <xahfi@localhost.localdomain>
    Co-authored-by: Shafi R <shafi.rafiq85@gmail.com>
    Co-authored-by: Herzen <herzen@herzens-air.home>
    
    * mid/staging
    
    * add mutators PLAINTEXT_Mutator XML_Mutator and function isXML
    
    ---------
    
    Co-authored-by: Herzen <herzen@herzens-air.home>
    Co-authored-by: xahfi <xahfi@localhost.localdomain>
    Co-authored-by: Shafi R <shafi.rafiq85@gmail.com>
    Co-authored-by: Herzen <herzen@Herzens-MacBook-Air.local>

--- src/process/file_types.py
+++ src/process/file_types.py
@@ -1,6 +1,8 @@
 import csv
 from io import BytesIO
 import json
+import xml.etree.ElementTree as ET
+
 
 def isCSV(input_raw: bytes) -> bool:
     """
@@ -41,6 +43,24 @@ def isJSON(input_raw: bytes) -> bool:
 
 # other matchers jpeg, xml, elf, pdf...
 
+def isXML(input_raw: bytes) -> bool:
+    """
+    Check if the given bytes represent an XML file.
+
+    :param data: bytes to check.
+    :return: True if data is an XML file, False otherwise.
+    """
+    try:
+        ET.fromstring(input_raw)
+        return True
+    except ET.ParseError:
+        return False
+    except Exception as e:
+        # Handle other potential exceptions, e.g., encoding issues
+        print(f"An error occurred: {e}")
+        return False
+
+
 def whichType(input):
     """
     Infer the file type of a given sample based on a series of matching functions.
@@ -51,7 +71,8 @@ def whichType(input):
         yield ("json", isJSON)
         # for later implementation
         # yield ("jpeg", jepg_checker)
-        # yield ("xml", xml_checker)
+        yield ("xml", isXML)
+
         # yield ("elf", elf_checker)
         # yield ("pdf", pdf_checker)
         
--- src/runner.py
+++ src/runner.py
@@ -9,15 +9,20 @@ class Manager:
 
     _MUTATORS = {
         "csv" : mutators.CSV_Mutator,
-        "json" : mutators.JSON_Mutator
+        "json" : mutators.JSON_Mutator,
+        "plaintext": mutators.PLAINTEXT_Mutator,
+        "xml": mutators.XML_Mutator
     }
     
     def __init__(self, binary, seed, times: int = 5000):
         self._num_runs = times
         self._current_checkpoint = 0
         
-        self._process_name = self._format_binary_path(binary)
-
+        if "./"  != binary[1:2]:
+            self._process_name = f"./{binary}"
+        else:
+            self._process_name = f"{binary}"
+        
         self._stop_flag = False
         
         try:
@@ -31,6 +36,7 @@ class Manager:
         self._file_type = process.whichType(self._input_file)
         self._fuzz = self._MUTATORS[self._file_type](self._input_file)
         self._init_process()
+
 
     def _format_binary_path(self, binary: str) -> str:
         """Ensure the binary path is correctly formatted."""
@@ -83,15 +89,16 @@ class Manager:
                 if exitcode < 0:  # Handle SIGFAULT
                     print(f"Program Crashed: exitcode = {exitcode}")
                     print(f"\tReason: {process.ExitCodes(-exitcode).name}")
-                    print(f"Dumped bad input report to {self._txt_name}_dump.txt")
+
+                    print(f"Dumped badinput to {self._txt_name}_dump.txt")
+
                     self._result_dump(input_bytes)
                     break
             except subprocess.TimeoutExpired:
                 print(f"{idx}: Timeout")
                 print(f"Dumped timeout report to {self._txt_name}_dump.txt")
                 self._result_dump(input_bytes)
-                break
-            
+                break        
             self._reset()
         
 
